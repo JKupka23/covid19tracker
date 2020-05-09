a669c3d7f8bcdcd5226eeff92297f5414dc79444
Adar Tulloch <atulloch@oberlin.edu>
2020-05-09 02:32:14 -0400

    Worked on script.js

--- script.js
+++ script.js
@@ -4,30 +4,18 @@ function initMap(){
         zoom:8,
         center:{lat:42.3601,lng:-71.0589}
     }
-
+    var positives;
+    for (int i=0; i<50; i++){
+      postives[i] = data[i].positive;
+    }
+    var negatives, deaths;
     // New map
     var map = new google.maps.Map(document.getElementById('map'), options);
-
-    /*
-    // Add marker
-    var marker = new google.maps.Marker({
-      position:{lat:42.4668,lng:-70.9495},
-      map:map,
-      icon:'https://developers.google.com/maps/documentation/javascript/examples/full/images/beachflag.png'
-    });
-    var infoWindow = new google.maps.InfoWindow({
-      content:'<h1>Lynn MA</h1>'
-    });
-    marker.addListener('click', function(){
-      infoWindow.open(map, marker);
-    });
-    */
-
     // Array of markers
     var markers = [
         {
             coords:{lat:32.3182,lng:-86.9023},
-            content:'<h1>Alabama</h1>'
+            content:'<h1>Alabama death: <span id="death"></span></h1>'
         },
         {
             coords:{lat:61.3707,lng:-152.4044},
@@ -227,10 +215,17 @@ function initMap(){
         },
         {
             coords:{lat:42.756,lng:-107.3025},
-            content:'<h1>Wyoming</h1>'
+            content:'Wyoming currently has ' + 1000000 + ' cases. There is a total recovery rate of ' + 100000 + ' people'
         }
     ];
 
+        const api_url = 'https://covidtracking.com/api/states';
+        async function getInfo() {
+          const response = await fetch(api_url);
+          const data = await response.json();
+          console.log(data);
+        }
+        getInfo();
     // Loop through markers
     for(var i = 0;i < markers.length;i++){
         // Add marker
@@ -244,6 +239,7 @@ function initMap(){
             map:map,
             //icon:props.iconImage
         });
+
 
         // Check for customicon
         if(props.iconImage){
