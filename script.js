bdfcd5301f0961b53c5157d20feb8f9e3f494227
Adar Tulloch <atulloch@oberlin.edu>
2020-05-09 16:37:53 -0400

    added second map option.

--- script.js
+++ script.js
@@ -1,8 +1,97 @@
 function initMap(){
-    // Map options
+    // Map options (options1 is just for fun)
+    var options1 = {
+      zoom:5,
+      center:{lat:39.8283,lng:-98.5795},
+      styles: [
+      {elementType: 'geometry', stylers: [{color: '#242f3e'}]},
+      {elementType: 'labels.text.stroke', stylers: [{color: '#242f3e'}]},
+      {elementType: 'labels.text.fill', stylers: [{color: '#746855'}]},
+      {
+        featureType: 'administrative.locality',
+        elementType: 'labels.text.fill',
+        stylers: [{color: '#d59563'}]
+      },
+      {
+        featureType: 'poi',
+        elementType: 'labels.text.fill',
+        stylers: [{color: '#d59563'}]
+      },
+      {
+        featureType: 'poi.park',
+        elementType: 'geometry',
+        stylers: [{color: '#263c3f'}]
+      },
+      {
+        featureType: 'poi.park',
+        elementType: 'labels.text.fill',
+        stylers: [{color: '#6b9a76'}]
+      },
+      {
+        featureType: 'road',
+        elementType: 'geometry',
+        stylers: [{color: '#38414e'}]
+      },
+      {
+        featureType: 'road',
+        elementType: 'geometry.stroke',
+        stylers: [{color: '#212a37'}]
+      },
+      {
+        featureType: 'road',
+        elementType: 'labels.text.fill',
+        stylers: [{color: '#9ca5b3'}]
+      },
+      {
+        featureType: 'road.highway',
+        elementType: 'geometry',
+        stylers: [{color: '#746855'}]
+      },
+      {
+        featureType: 'road.highway',
+        elementType: 'geometry.stroke',
+        stylers: [{color: '#1f2835'}]
+      },
+      {
+        featureType: 'road.highway',
+        elementType: 'labels.text.fill',
+        stylers: [{color: '#f3d19c'}]
+      },
+      {
+        featureType: 'transit',
+        elementType: 'geometry',
+        stylers: [{color: '#2f3948'}]
+      },
+      {
+        featureType: 'transit.station',
+        elementType: 'labels.text.fill',
+        stylers: [{color: '#d59563'}]
+      },
+      {
+        featureType: 'poi.medical',
+        elementType: 'labels.text.fill',
+        stylers: [{color: '#ff5050'}]
+      },
+      {
+        featureType: 'water',
+        elementType: 'geometry',
+        stylers: [{color: '#17263c'}]
+      },
+      {
+        featureType: 'water',
+        elementType: 'labels.text.fill',
+        stylers: [{color: '#515c6d'}]
+      },
+      {
+        featureType: 'water',
+        elementType: 'labels.text.stroke',
+        stylers: [{color: '#17263c'}]
+      }
+    ]
+  }
     var options = {
-        zoom:5,
-        center:{lat:39.8283,lng:-98.5795}
+      zoom:5,
+      center:{lat:39.8283,lng:-98.5795}
     }
     // New map
     var map = new google.maps.Map(document.getElementById('map'), options);
@@ -232,8 +321,7 @@ function initMap(){
     function addMarker(props){
         var marker = new google.maps.Marker({
             position:props.coords,
-            map:map,
-            //icon:props.iconImage
+            map:map
         });
 
 
@@ -251,6 +339,10 @@ function initMap(){
 
             marker.addListener('click', function(){
                 infoWindow.open(map, marker);
+            });
+            //Wait before user moves their cursor
+            marker.addListener('mouseout', function(){
+              setTimeout(() => { infoWindow.close(map,marker); }, 5000);
             });
         }
     }
